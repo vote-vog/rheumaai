09c0e3428c9236fb418168b8f8a436c14c8fd630
vote-vog <gaara5249@gmail.com>
2025-08-26 23:56:17 +0500

    Create index.tsx

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>RheumaAI MVP!</Text>
+      <Text>Стартовая страница приложения</Text>
+    </View>
+  );
+}
