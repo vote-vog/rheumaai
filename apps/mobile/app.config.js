df7f7647e1d2005f4d895654eae8321242f79f8e
vote-vog <gaara5249@gmail.com>
2025-08-27 01:34:59 +0500

    Update app.config.js

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,12 +1,42 @@
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    supabaseUrl: process.env.SUPABASE_URL,
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
-    openaiApiKey: process.env.OPENAI_API_KEY,
-  },
-});
+export default {
+  expo: {
+    name: "RheumaAI",
+    slug: "rheumaai",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    scheme: "rheumaai",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.yourcompany.rheumaai"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      package: "com.yourcompany.rheumaai"
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+      bundler: "metro"
+    },
+    plugins: [
+      "expo-router"
+    ],
+    extra: {
+      supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+      openaiApiKey: process.env.OPENAI_API_KEY
     }
   }
 };
