6d9a6c94b23acde82531c8ac9c0277f295abd99d
vote-vog <gaara5249@gmail.com>
2025-08-27 02:18:43 +0500

    Update app.config.js

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -4,25 +4,32 @@ export default {
     slug: "rheumaai",
     version: "1.0.0",
     orientation: "portrait",
+    icon: "./assets/icon.png",
     userInterfaceStyle: "light",
     scheme: "rheumaai",
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: ["**/*"],
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.yourcompany.rheumaai"
     },
     android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
       package: "com.yourcompany.rheumaai"
     },
     web: {
+      favicon: "./assets/favicon.png",
       bundler: "metro"
     },
-    plugins: [
-      "expo-router"
-    ],
-    // УБИРАЕМ runtimeVersion полностью!
+    plugins: ["expo-router"],
+    // УБИРАЕМ runtimeVersion и sdkVersion!
     extra: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
