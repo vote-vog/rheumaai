ac7614ac5ba2db3e19a68c93526b1d83597d7113
vote-vog <gaara5249@gmail.com>
2025-08-28 00:42:47 +0500

    Update metro.config.js

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,11 +1,11 @@
-// apps/mobile/metro.config.js
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Убираем проблемный transformer
 config.transformer = {
   ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-sass-transformer'),
+  // babelTransformerPath: require.resolve('react-native-sass-transformer'), // УДАЛИТЬ ЭТУ СТРОКУ
 };
 
 config.resolver = {
