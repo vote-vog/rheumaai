793571ad6cd2cbbf77a6b146a9cf5caa47c03431
vote-vog <gaara5249@gmail.com>
2025-08-28 00:19:26 +0500

    Update metro.config.js

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,16 +1,16 @@
 // apps/mobile/metro.config.js
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
+const config = getDefaultConfig(__dirname);
 
-const config = getDefaultConfig(projectRoot);
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-sass-transformer'),
+};
 
-config.watchFolders = [workspaceRoot];
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
+config.resolver = {
+  ...config.resolver,
+  sourceExts: [...config.resolver.sourceExts, 'scss', 'sass'],
+};
 
 module.exports = config;
